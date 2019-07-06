5eb11a0ea34aa6628bd5c9d664a219a719c7221c
adgwr <imliyc@sina.com>
2019-07-07 01:27:29 +0800

    合并注销功能，修复分页的Bug

--- src/main/java/com/adgwr/online/ordering/system/customer/controller/ShowCartController.java
+++ src/main/java/com/adgwr/online/ordering/system/customer/controller/ShowCartController.java
@@ -1,7 +1,16 @@
 package com.adgwr.online.ordering.system.customer.controller;
 
+import com.adgwr.online.ordering.system.customer.service.CustomerService;
+import com.adgwr.online.ordering.system.domain.Food;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * @author Administrator
@@ -9,4 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ShowCartController {
 
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping(value = "showCart",method = RequestMethod.GET)
+    public String showCart(){
+        return "showCart";
+    }
+
+    @RequestMapping(value = "GetFoods", method = RequestMethod.POST)
+    public String getFoods(
+            @RequestParam(value = "pn", defaultValue = "1") Integer pn,
+            String searchContent,
+            Model model) {
+        PageHelper.startPage(pn, 8);
+        PageHelper.orderBy("emp_id asc");
+        List<Food> foods = customerService.getFoods(searchContent);
+        model.addAttribute("foods", foods);
+        return "menu";
+    }
 }
